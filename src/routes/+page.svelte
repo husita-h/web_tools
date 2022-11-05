28c93609fb14188dcfaf710edf8afc3a655288ce
husita-h <taisei.fukushi1129@gmail.com>
2022-11-05 19:29:49 +0900

    ページの追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Top Page</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="http://localhost:5173/about">link</a> to About Page</p>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { writable } from 'svelte/store';
+
+	const progress = writable(0);
+</script>
+
+<h1>About Page</h1>
+
+<progress value={$progress}></progress>
+
+<button on:click="{() => progress.set(0)}">
+	0%
+</button>
+
+<button on:click="{() => progress.set(0.25)}">
+	25%
+</button>
+
+<button on:click="{() => progress.set(0.5)}">
+	50%
+</button>
+
+<button on:click="{() => progress.set(0.75)}">
+	75%
+</button>
+
+<button on:click="{() => progress.set(1)}">
+	100%
+</button>
+
+<p>Back <a href="http://localhost:5173/">link</a> to Top Page</p>
+
+<style>
+	progress {
+		display: block;
+		width: 100%;
+	}
+</style>
