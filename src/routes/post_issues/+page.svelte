c34d7af3ac77d1d771c229863f9a4b0ca7d19e5d
husita-h <taisei.fukushi1129@gmail.com>
2022-11-18 01:56:21 +0900

    環境変数の読み込み設定

--- src/routes/post_issues/+page.svelte
+++ src/routes/post_issues/+page.svelte
@@ -1,25 +1,26 @@
 <script>
+    let result;
+
     async function testFunc1(){
-        const url = "http://localhost:9292/sub";
+        // https://scottspence.com/posts/sveltekit-environment-variables-with-the-sveltekit-env-module
+        // const url = import.meta.env.MOCK_API_ENDPOINT;
+        const url = import.meta.env.VITE_MOCK_API_ENDPOINT;
+        // const url = process.env['MOCK_API_ENDPOINT']
         const response = await fetch(url, { mode: "no-cors" })
-        console.log(response)
-        return response;
+        console.log(url);
+        console.log(response);
+        result = response;
+        return;
     }
-    async function testFunc2(){
-        const url = "https://status.cloud.google.com/incidents.json";
-        const response = await fetch(url).then((response) => console.log(response));
-        console.log(response)
-        return response;
-    }
-    async function testFunc3(){
-        const url = "https://localhost:9292/sub";
-        await fetch(url, { mode: "no-cors" }).then((response) => console.log(response));
-        return
+    function clear(){
+        result = null;
+        return;
     }
 </script>
 
 <h1>Post Issues</h1>
 
 <button on:click={testFunc1}>testFunc1</button>
-<button on:click={testFunc2}>testFunc2</button>
-<button on:click={testFunc3}>testFunc3</button>
+<button on:click={clear}>clear</button>
+
+<div>result: {result || ""}</div>
